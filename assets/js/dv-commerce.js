e9cd22613686d4828576934ea9447c011e203b1b
paul karikari <paulkarikari1@gmail.com>
2017-07-26 00:32:14 +0000

    Initial commit

--- assets/js/dv-commerce.js
+++ assets/js/dv-commerce.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
 
-    var menu = $('.menu');
-    var origOffsetY = menu.offset().top;
+    /* var menu = $('.menu');
+     var origOffsetY = menu.offset().top;
 
-    function scroll() {
-        if ($(window).scrollTop() >= origOffsetY) {
-            $('.menu').addClass('sticky');
-            $('.content').addClass('menu-padding');
-        } else {
-            $('.menu').removeClass('sticky');
-            $('.content').removeClass('menu-padding');
-        }
-    }
-    document.onscroll = scroll;
+     function scroll() {
+         if ($(window).scrollTop() >= origOffsetY) {
+             $('.menu').addClass('sticky');
+             $('.content').addClass('menu-padding');
+         } else {
+             $('.menu').removeClass('sticky');
+             $('.content').removeClass('menu-padding');
+         }
+     }
+     document.onscroll = scroll;*/
 
 });
